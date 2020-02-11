e30a9ac44b0ef61b147ea1dd32c79faaaf313092
Isaí Hernández <iasiher13@gmail.com>
2020-02-10 21:05:33 -0600

    Propiedades computadas y métodos

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const app = new Vue({
+    el:'#app',
+    data:{
+       message: 'Hello vue',
+       sum : 3 + 6,
+       value : 5
+    },
+    computed: {
+        // a computed getter
+        reversedMessage: function () {
+          // `this` points to the vm instance
+          return this.message.split('').reverse().join('')
+        }
+      },
+      methods:{
+          calculateSum( value1, value2){
+              return value1 + value2;
+          }
+      }
+});
